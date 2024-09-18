239f61e606d4046885bd92fd546d6b1451893c64
Alireza moosavi <alirezamoosavinia@yahoo.com>
2024-09-18 17:12:16 +0330

    update plugins

--- lua/alireza/plugins/lsp/lspconfig.lua
+++ lua/alireza/plugins/lsp/lspconfig.lua
@@ -114,7 +114,7 @@ return {
 			on_attach = on_attach,
 		})
 		-- configure typescript server with plugin
-		lspconfig["tsserver"].setup({
+		lspconfig["ts_ls"].setup({
 			capabilities = capabilities,
 			on_attach = on_attach,
 		})
